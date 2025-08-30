753c76ff50b953b54d7e90461f13a910d568833c
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2025-08-30 12:56:10 -0600

    Laravel Breeze y mostrar productos en la ruta /products

---
+++ app/Livewire/Products.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use App\Models\ProductCategory;
+use Livewire\Component;
+
+class Products extends Component
+{
+    public $categories;
+    public $selectedCategory='all';
+    public $searchQuery = '';
+    public $products;
+
+    public function mount()
+    {
+        $this->categories = ProductCategory::with('products')->get();
+        $this->products = Product::with('categories')->get();
+    }
+    public function render()
+    {
+        return view('livewire.products', [
+            'records' => $this->products,
+        ])->layout('layouts.guest');
+    }
+
+    public function filterByCategory($categoryId)
+    {
+        if ($categoryId === 'all') {
+            $this->products = Product::with('categories')->get();
+            $this->selectedCategory = null;
+        } else {
+            $category = ProductCategory::with('products')->find($categoryId);
+            if ($category) {
+                $this->products = $category->products;
+                $this->selectedCategory = $category->name;
+            }
+        }
+    }
+
+    public function updatedSearchQuery()
+    {
+        $this->products = Product::where('name', 'like', '%' . $this->searchQuery . '%')->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Livewire\Products;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -8,10 +11,21 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    if (Auth::check()) {
+        if (Auth::check()) {
         if (Auth::user()->isAdmin()) {
             return redirect()->to('/admin');
         }
     }
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/productos',Products::class)->name('products');
+
+require __DIR__.'/auth.php';
