2c2c3594f38436c6d06155902c7af9a0822f9eaf
gertrukas <gerardo.trujillo@gmail.com>
2025-11-12 11:06:26 -0600

    hero section

--- resources/views/layouts/principal-hero-section.blade.php
+++ resources/views/layouts/principal-hero-section.blade.php
@@ -1,8 +1,18 @@
- <section
-     class="mt-2 wrapper grid gap-2 grid-cols-1justify-items-center footer-area md:footer-area-md md:grid-cols-1 md:justify-items-stretch md:text-center">
-     <div class="flex flex-wrap gap-2 justify-center w-full [grid-area:social-media] md:justify-start">
-         <img src="{{ asset('images/hero-section.jpg') }}"
-          alt="wave"
-          class="w-full -mt-1 mb-4">
-     </div>
- </section>
+
+ <section>
+ <div class="relative isolate px-6 pt-14 lg:px-8 ]" style="background-image: url('{{ asset('images/hero-section.jpg') }}')" >
+    
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      
+      <div class="text-center">
+        <h1 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Tenemos los kits mas completos para tu proyecto</h1>
+        <p class="mt-8 text-lg font-medium text-pretty text-white sm:text-xl/8">gran variedad de componentes y productos.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Ver kits</a>
+          <a href="#" class="text-sm/6 font-semibold text-white">Conocer mas<span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+    </div>
+   
+  </div>
+</section>
--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,15 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'XsBeta') }}</title>
 
     <link rel="icon" href="{{ asset('images/favicon-32x32.png') }}" sizes="any">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;700&display=swap" rel="stylesheet">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -24,15 +27,17 @@
 </head>
 
 <body class="font-principal bright-red flex flex-col min-h-screen">
-    <div class="grow">
+    
+
         @include('layouts.principal-header')
+
         @include('layouts.principal-hero-section')
 
 
         <div class="min-h-screen">
             {{ $slot }}
         </div>
-    </div>
+    
 
     @include('layouts.principal-footer')
 
